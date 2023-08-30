c98a165c09c3d5054106fcdf8e73e6e0645b9ef9
thomasIRA <thomas.al@live.ca>
2023-08-30 15:25:13 -0400

    staion switch push

--- index.php
+++ index.php
@@ -1,13 +1,47 @@
 <?php
 include "./partial/school-header.php";
 include "./partial/navigation.php";
-require "./lib/View.php";
-
 require "./class/Stamp.php";
 require "./crud/crud.php";
 
+$tableOg = "stamp";
+$tablesMg = [
+    "aspect",
+    "category"
+];
+$targets = [
+    "stamp.id",
+    "stamp.name as name",
+    "stamp.year",
+    "stamp.origin",
+    "stamp.description",
+    "aspect.name as aspect", 
+    "category.name as category"
+];
 $crud = new Crud("stamp");
-$stamps = $crud->read("stamp", ["stamp.id as stamp_id, stamp.name as stamp_name, condition.id as condition_id, stamp.name, condition.name"], ["condition", "category"]);
+$stamps = $crud->read($tableOg, $targets, $tablesMg);
+?>
+
+<main>
+
+<?php foreach ($stamps as $stamp) : ?>
+    <article class="stamp-card">
+        <hgroup>
+            <h2><?= $stamp["name"] ?></h2>
+            <?php if ($stamp["description"]): ?>
+            <p><?= $stamp["description"] ?></p>
+            <?php endif; ?>
+        </hgroup>
+        <div>
+
+        </div>
+    </article>
 
 
-include "./partial/school-footer.php";
+<?php endforeach; ?>
+
+
+</main>
+
+
+<?php include "./partial/school-footer.php"; ?>
