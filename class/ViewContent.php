944b093aa1362c8ad8253a480801fe5989227290
thomasIRA <thomas.al@live.ca>
2023-09-04 22:51:01 -0400

    fucked, will fix tomorrow

--- class/ViewContent.php
+++ class/ViewContent.php
@@ -25,66 +25,7 @@ class ViewContent {
         <?php
     }
 
-    static public function panel($data) {
-        $users = $data["users"];
-        $stamps = $data["stamps"];
-        $aspects = $data["aspects"];
-        $categories = $data["categories"];
 
-        $msg = null;
-        if (isset($_GET["msg"]) && $_GET["msg"] != null) {
-            if ($_GET["msg"] == 1) $msg = "<p>user created</p>";
-            if ($_GET["msg"] == 2) $msg = "<p>stamp created</p>";
-            if ($_GET["msg"] == 3) $msg = "<p>aspect created</p>";
-            if ($_GET["msg"] == 4) $msg = "<p>category created</p>";
-        }
-        ?>
-        <?= $msg ?>
-        <main>
-            <header>
-                <h2>Panel</h2>
-            </header>
-            <div>
-                <section>
-                    <h3>All users</h3>
-                    <ul>
-                        <?php foreach ($users as $user) :?>
-                        <li><a href="./user-show.php?id=<?= $user["id"] ?>"><?= $user["name"] ?></a></li>
-                        <?php endforeach ?>
-                    </ul>
-                    <a href="./user-create.php" class="button">create user</a>
-                </section>
-                <section>
-                    <h3>All stamps</h3>
-                    <ul>
-                    <?php foreach ($stamps as $stamp) :?>
-                        <li><a href="./stamp-show.php?id=<?= $stamp["id"] ?>"><?= $stamp["name"] ?></a></li>
-                        <?php endforeach ?>
-                    </ul>
-                    <a href="./stamp-create.php" class="button">add stamp</a>
-                </section>
-                <section>
-                    <h3>All aspects</h3>
-                    <ul>
-                    <?php foreach ($aspects as $aspect) :?>
-                        <li><?= $aspect["aspect"] ?></a></li>
-                        <?php endforeach ?>
-                    </ul>
-                    <a href="./aspect-create.php" class="button">add aspect</a>
-                </section>
-                <section>
-                    <h3>All categories</h3>
-                    <ul>
-                    <?php foreach ($categories as $category) :?>
-                        <li><?= $category["category"] ?></a></li>
-                        <?php endforeach ?>
-                    </ul>
-                    <a href="./category-create.php" class="button">add category</a>
-                </section>
-            </div>
-        </main>
-        <?php
-    } 
 
 
 
